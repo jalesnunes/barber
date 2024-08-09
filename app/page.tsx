eb2d290edac4abf48b3bea105c2a007b5e4bca5f
Jales Nunes <jales.nunes@hotmail.com>
2024-08-08 18:42:46 -0600

    added chadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,57 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
-  return <Button>teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Hi, Jales!</h2>
+        <p>Friday, August 8</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mx-0 mt-6 h-[150px] w-full">
+          <Image
+            alt="book with the best barbers"
+            src="/banner-01.png"
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmed</Badge>
+              <h3 className="font-bold">Hair Cut</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid px-5">
+              <p className="text-sm">August</p>
+              <p className="text-2xl">07</p>
+              <p className="text-sm">9:45pm</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
