4e4ee10706c5876f19fbd20d9f60959e0fd704e8
Jales Nunes <jales.nunes@hotmail.com>
2024-09-16 15:42:52 -0600

    added some contents to barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,19 +1,50 @@
+import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { Barbershop } from "@prisma/client"
+import { ChevronLeftIcon, MenuIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
 
 interface BarbershopPageProps {
-    params: Barbershop
+  params: Barbershop
 }
 
-const BarbershopPage = async ( { params } : BarbershopPageProps ) => {
-    const barbershop = await db.barbershop.findUnique({
-        where: {
-            id: params.id
-        }
-    })
-    return(
-        <h1>{barbershop?.name}</h1>
-    )
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          alt={barbershop?.name}
+          src={barbershop?.imageUrl}
+          fill
+          className="object-cover"
+        />
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default BarbershopPage
