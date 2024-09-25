0cc9d2deb120f8d964219d79b590f45f78a109ec
Jales Nunes <jales.nunes@hotmail.com>
2024-09-25 13:04:46 -0600

    added next auth setup

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image"
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+
+import googleIconImg from "../../public/googleIcon.svg"
 
 export default function Sidebar() {
   return (
@@ -13,15 +23,38 @@ export default function Sidebar() {
         <SheetTitle className="text-left text-sm">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Hey, login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <LogInIcon size="icon" />
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Sign in</DialogTitle>
+              <DialogDescription>
+                Sign in using your Google account
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image src={googleIconImg} alt="sign in with google" width={18} height={18}/>
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="https://avatar.iran.liara.run/public/20" />
         </Avatar>
 
         <div>
           <p className="font-bold">Jales Nunes</p>
           <p className="text-xs">jales@jales.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
